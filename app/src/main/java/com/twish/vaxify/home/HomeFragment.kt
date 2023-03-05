4bd9d24ff89a81a66ec748fdba6b066acc502c4e
twish-254 <caroline1gatwiri@gmail.com>
2023-03-05 18:12:14 +0300

    add spinner functions

--- app/src/main/java/com/twish/vaxify/home/HomeFragment.kt
+++ app/src/main/java/com/twish/vaxify/home/HomeFragment.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.twish.vaxify.databinding.FragmentHomeBinding
 
@@ -12,9 +15,7 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
@@ -23,18 +24,25 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /**val spinner = binding.location
+        val spinner = binding.location
         // Initialize the adapter with data for the spinner
         val spinnerAdapter = ArrayAdapter(
             requireContext(),
-            R.layout.support_simple_spinner_dropdown_item,
+            androidx.transition.R.layout.support_simple_spinner_dropdown_item,
             listOf(
-                "Turkana County",
                 "Marsabit County",
-                "Samburu County",
-                "Isiolo County ",
+                "Mandera County",
                 "Wajir County",
-                "Mandera County"
+                "Turkana County",
+                "Samburu County",
+                "Isiolo County",
+                "Garissa County",
+                "Kitui County",
+                "Tana River County",
+                "Laikipia County",
+                "Narok County",
+                "Kajiado County",
+                "West Pokot County"
             )
         )
 
@@ -46,10 +54,7 @@ class HomeFragment : Fragment() {
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
             ) {
                 // Handle the selection
                 val selectedItem = spinnerAdapter.getItem(position)
@@ -61,5 +66,50 @@ class HomeFragment : Fragment() {
                 // Do nothing*/
             }
         }
+
+
+    val spinnerAge = binding.age
+    // Initialize the adapter with data for the spinner
+    val ageSpinnerAdapter = ArrayAdapter(
+        requireContext(),
+        androidx.transition.R.layout.support_simple_spinner_dropdown_item,
+        listOf(
+            "Newborn",
+            "6 weeks",
+            "10 weeks",
+            "14 weeks",
+            "9 months",
+            "18 months",
+            "6 years",
+            "10 years"
+        )
+    )
+
+    // Set the dropdown style
+    spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+    // Attach the adapter to the spinner
+    spinnerAge.adapter = ageSpinnerAdapter
+
+    spinnerAge.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>?, view: View?, position: Int, id: Long
+        ) {
+            // Handle the selection
+            val selectedItem = ageSpinnerAdapter.getItem(position)
+            Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // Do nothing
+        }
+    }
+    }
+}
+
+
+
+
 
 
